bb2751c993f775b043ea1532250fb0b72c4a29bf
Tongbo Sui <beanixster@gmail.com>
2015-10-23 10:25:37 -0400

    debug view fix

--- glsl/deferred/debug.frag.glsl
+++ glsl/deferred/debug.frag.glsl
@@ -29,7 +29,7 @@ void main() {
     // TODO: Extract needed properties from the g-buffers into local variables
     // These definitions are suggested for starting out, but you will probably want to change them.
     vec3 pos = gb0.xyz;     // World-space position
-    vec3 geomnor = gb1.xyz;  // Normals of the geometry as defined, without normal mapping
+    //vec3 geomnor = gb1.xyz;  // Normals of the geometry as defined, without normal mapping
     vec3 colmap = gb2.xyz;  // The color map - unlit "albedo" (surface color)
     //vec3 normap = gb3.xyz;  // The raw normal map (normals relative to the surface they're on)
     //vec3 nor = applyNormalMap(geomnor, normap);     // The true normals as we want to light them - with the normal map applied to the geometry normals (applyNormalMap above)
@@ -44,9 +44,9 @@ void main() {
         gl_FragColor = vec4(abs(nor), 1.0);
     } else if (u_debug == 3) {
         gl_FragColor = vec4(colmap, 1.0);
-    } else if (u_debug == 4) {
+    } /*else if (u_debug == 4) {
         gl_FragColor = vec4(normap, 1.0);
-    } /*else if (u_debug == 5) {
+    } else if (u_debug == 5) {
         gl_FragColor = vec4(abs(nor), 1.0);
     }*/ else {
         gl_FragColor = vec4(1, 0, 1, 1);
