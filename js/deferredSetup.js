d6c7f3d03f0ba37b14b825817cc65382507180e4
Levi Cai <levi@levicai.com>
2015-10-27 16:41:35 -0400

    Added more images, removed a g_buffer

--- js/deferredSetup.js
+++ js/deferredSetup.js
@@ -9,7 +9,7 @@
     R.pass_post2 = {};
     R.lights = [];
 
-    R.NUM_GBUFFERS = 4;
+    R.NUM_GBUFFERS = 3;
 
     /**
      * Set up the deferred pipeline framebuffer objects and textures.
@@ -27,7 +27,7 @@
     R.light_max = [14, 18, 6];
     R.light_dt = -0.03;
     R.LIGHT_RADIUS = 4.0;
-    R.NUM_LIGHTS = 10; // TODO: test with MORE lights!
+    R.NUM_LIGHTS = 150; // TODO: test with MORE lights!
     var setupLights = function() {
         Math.seedrandom(0);
 
